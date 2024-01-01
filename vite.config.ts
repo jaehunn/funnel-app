5a4c01a8b36878ce78b385f036c26212ca4ff1ca
Jaehun <qkdwogns98@gmail.com>
2024-01-01 23:07:27 +0900

    Path Alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vitejs.dev/config/
+/** @see https://ko.vitejs.dev/guide/ */
 export default defineConfig({
   plugins: [react()],
-})
+
+  /** @see {docs} https://ko.vitejs.dev/config/server-options.html#server-hmr */
+  server: {
+    host: true,
+    open: "/",
+    port: 3000,
+  },
+
+  resolve: {
+    /** @see https://ko.vitejs.dev/config/shared-options.html#resolve-alias */
+    alias: [
+      {
+        find: "@",
+        replacement: path.resolve(__dirname, "src"),
+      },
+    ],
+  },
+});
